2bb7d20ebce9fd6ef6c19b4e65f80bdd81ac4738
Herzog <charlotte.herzog@telecom-paristech.fr>
2014-03-20 15:09:23 +0100

    RDV thomas robert : ça marche !!!

--- src/communication/ConnectedThread.java
+++ src/communication/ConnectedThread.java
@@ -2,6 +2,9 @@ package communication;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.Buffer;
 
@@ -13,18 +16,22 @@ import android.util.Log;
 public class ConnectedThread extends Thread {
 	private final BluetoothSocket socket;
 	private final OutputStream stream;
+	private final BufferedReader RXstream;
 	//Peut-être mettre un attribut supplémentaire = buffer ?
 	
 	public ConnectedThread(BluetoothSocket bs) {
 		socket = bs;
         System.out.println("La Socket est " + socket);
 		OutputStream toolStream = null;
+		InputStream RXstreamtmp=null;
 		try{
 			toolStream = socket.getOutputStream();
+			RXstreamtmp= socket.getInputStream();
 			} catch(IOException e){
 				MainActivity.problemeDeConnexion();
 			}
 		stream = toolStream;
+		RXstream=new BufferedReader(new InputStreamReader(RXstreamtmp));
 		System.out.println("Au début de CtedThread, le stream est " + stream);
 		
 		//Se signaler à la méthode main
@@ -35,7 +42,12 @@ public class ConnectedThread extends Thread {
 	
 	public void run() {
 		this.transmettre(new byte[] {1,2,1,1});
-		
+		try {
+			System.out.println(this.RXstream.readLine()+" Fin lecture\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("erreur lecture");
+		}
 		System.out.println("Dans run(), le stream est " + stream);
 	}
 	
