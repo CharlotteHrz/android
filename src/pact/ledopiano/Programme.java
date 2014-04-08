f87661cf9e5b279be9e86124274dacd0272342ee
de-bray <hortense.de-bray@telecom-paristech.fr>
2014-04-08 15:20:32 +0200

    Intégration

---
+++ src/pact/ledopiano/Programme.java
@@ -0,0 +1,29 @@
+package pact.ledopiano;
+
+import harmonie.Commande;
+
+import java.util.ArrayList;
+
+import Analyse.Chroma;
+import lecture.FichierAudio;
+
+public class Programme {
+
+
+
+public ArrayList<Commande> general (String name) throws Exception {
+	
+FichierAudio fichier = new FichierAudio(name);
+//name est le nom du fichier pické
+ArrayList<Chroma> tableChroma = fichier.transformeeDeFourier();
+analyseChroma
+
+
+
+	
+	
+	
+	
+	
+}
+}
